0fef2c375156bc2054f3e1b8c575df4d9071cc32
kate <46572320+katelmao@users.noreply.github.com>
2023-10-22 21:00:57 +0100

    feat: dynamic cosmetic items

--- assets/tag/models/item/create.py
+++ assets/tag/models/item/create.py
@@ -36,6 +36,39 @@ while index <= amount:
 		}}
 	}}
 }}''')
+    elif selection == 'bought':
+        with open(f'not_bought_{prefix}_{index}.json', 'w') as file:
+            file.write(f'''{{
+	"parent": "minecraft:item/generated_18",
+	"textures": {{
+		"layer0": "tag:item/{prefix}_{index}",
+        "layer1": "tag:item/not_bought"
+	}},
+	"display": {{
+		"thirdperson_righthand": {{
+			"scale": [0, 0, 0]
+		}},
+		"thirdperson_lefthand": {{
+			"scale": [0, 0, 0]
+		}},
+		"firstperson_righthand": {{
+			"scale": [0, 0, 0]
+		}},
+		"firstperson_lefthand": {{
+			"scale": [0, 0, 0]
+		}},
+		"ground": {{
+			"scale": [0, 0, 0]
+		}},
+		"head": {{
+			"scale": [0, 0, 0]
+		}},
+		"fixed": {{
+			"scale": [1, 1, 1],
+			"rotation": [0, -180, 0]
+		}}
+	}}
+}}''')
     else:
         with open(f'sel_{prefix}_{index}.json', 'w') as file:
             file.write(f'''{{
